21ef786a1dd8b0058c4aa60f7f0b16afaf85d5a1
Matheus Dutra <matheusdutra0207@gmail.com>
2022-09-01 11:49:58 -0300

    fix:  changing is_message_type to Pose

--- reconstruction_3d/src/publishToROS.py
+++ reconstruction_3d/src/publishToROS.py
@@ -11,7 +11,7 @@ from tf.transformations import quaternion_from_euler
 
 # IS
 from is_wire.core import Channel, Subscription
-from is_msgs.camera_pb2 import FrameTransformation
+from is_msgs.common_pb2 import Pose
 
 #  my own
 from streamChannel import StreamChannel
@@ -20,13 +20,11 @@ from streamChannel import StreamChannel
 def getPoseReconstruction():
     message = channel_recontruction.consume_last()
     if type(message) != bool:
-        f = message.unpack(FrameTransformation)
-        tf = f.tf.doubles
-        x_recontruction = tf[0]
-        y_recontruction = tf[1]
-        roll_rad_recontruction = tf[3]
-
-        return np.array([x_recontruction, y_recontruction, roll_rad_recontruction])
+        pose = message.unpack(Pose)
+        x_recontruction = pose.position.x
+        y_recontruction = pose.position.y
+        yaw_rad_recontruction = pose.orientation.yaw
+        return np.array([x_recontruction, y_recontruction, yaw_rad_recontruction])
     else:
         return message
 
@@ -66,7 +64,7 @@ if __name__ == '__main__':
 
     channel_recontruction = StreamChannel(broker_uri)
     subscription = Subscription(channel_recontruction)
-    subscription.subscribe(topic=f"localization.{aruco_id}.aruco")
+    subscription.subscribe(topic=f"reconstruction.{aruco_id}.ArUco")
 
     try:
         publishToRos()
